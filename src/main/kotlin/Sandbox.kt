9ab841a59289ae6766b64cc2b3217abe024ef87e
meo209 <redslyice@outlook.de>
2024-05-02 23:14:38 +0200

    refactor: Base for rewrite

---
+++ src/main/kotlin/Sandbox.kt
@@ -0,0 +1,17 @@
+import org.openartifact.artifact.core.Application
+
+class Sandbox : Application() {
+
+    override fun init() {
+
+    }
+
+    override fun update() {
+
+    }
+
+    override fun shutdown() {
+
+    }
+
+}
