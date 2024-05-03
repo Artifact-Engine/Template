db32c998759da2cfaca69f1e6d33a784a29e682b
jkfkfnkg <henkel@outlook.de>
2024-05-03 13:09:50 +0200

    fix: Shader link fix, OpenGL core profile

--- src/main/kotlin/Sandbox.kt
+++ src/main/kotlin/Sandbox.kt
@@ -3,6 +3,7 @@ import org.lwjgl.opengl.GL46.*
 import org.openartifact.artifact.Entry
 import org.openartifact.artifact.core.Application
 import org.openartifact.artifact.core.Artifact
+import org.openartifact.artifact.graphics.platform.opengl.Shader
 import org.openartifact.artifact.input.KeyConstants.KEY_LEFT_CONTROL
 import org.openartifact.artifact.input.KeyConstants.KEY_Q
 import org.openartifact.artifact.input.createKeyInputMap
@@ -14,10 +15,6 @@ class Sandbox : Application() {
     private val keyInputMap = createKeyInputMap {
         KEY_LEFT_CONTROL with KEY_Q to { GLFW.glfwSetWindowShouldClose(Artifact.instance.window.handle, true) }
     }
-
-    private var vertexArray = 0
-    private var vertexBuffer = 0
-    private var indexBuffer = 0
 
     override fun init() {
         logger.info("Sandbox init")
@@ -46,11 +43,40 @@ class Sandbox : Application() {
         val indices = intArrayOf(0, 1, 2)
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW)
 
+        val vertexSource = """
+            #version 330 core
+            
+            layout(location = 0) in vec3 a_Position;
+            
+            out vec3 v_Position;
+            
+            void main() {
+                v_Position = a_Position;
+                gl_Position = vec4(a_Position, 1.0);
+            }
+            
+        """.trimIndent()
 
+        val fragmentSource = """
+            #version 330 core
+            
+            layout(location = 0) out vec4 color;
+            
+            in vec3 v_Position;
+            
+            void main() {
+                color = vec4(v_Position * 0.5 + 0.5, 1.0);
+            }
+            
+        """.trimIndent()
+
+        shader = Shader(vertexSource, fragmentSource)
     }
 
     override fun update() {
         keyInputMap.process()
+
+        shader!!.bind()
 
         glClearColor(0.1f, 0.1f, 0.1f, 1f)
         glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
