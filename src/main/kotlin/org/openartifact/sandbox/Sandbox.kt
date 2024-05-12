83010dd7ec0190ef0962b9312b6fb2a476c29027
meo209 <redslyice@outlook.de>
2024-05-12 22:50:58 +0200

    feat: 3D Cube and depth-testing

--- src/main/kotlin/org/openartifact/sandbox/Sandbox.kt
+++ src/main/kotlin/org/openartifact/sandbox/Sandbox.kt
@@ -23,7 +23,7 @@ import org.openartifact.artifact.input.with
 class Sandbox : Application(
     RenderAPI.OpenGL,
     WindowConfig(
-        640, 360, "Sandbox"
+        854, 480, "Sandbox"
     )
 ) {
 
@@ -47,15 +47,58 @@ class Sandbox : Application(
         // Rectangle
         rectVertexArray = renderer.choose<IVertexArray>().create()
 
-        val rectVertices = floatArrayOf(
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.5f, 0.5f, 0.0f,
-            -0.5f, 0.5f, 0.0f
+        val vertices = floatArrayOf(
+            -1.0f, -1.0f, -1.0f, // 0
+            -1.0f, -1.0f,  1.0f, // 1
+            -1.0f,  1.0f,  1.0f, // 2
+            1.0f,  1.0f, -1.0f, // 3
+            -1.0f, -1.0f, -1.0f, // 4
+            -1.0f,  1.0f, -1.0f, // 5
+            1.0f, -1.0f,  1.0f, // 6
+            -1.0f, -1.0f, -1.0f, // 7
+            1.0f, -1.0f, -1.0f, // 8
+            1.0f,  1.0f, -1.0f, // 9
+            1.0f, -1.0f, -1.0f, // 10
+            -1.0f, -1.0f, -1.0f, // 11
+            -1.0f, -1.0f, -1.0f, // 12
+            -1.0f,  1.0f,  1.0f, // 13
+            -1.0f,  1.0f, -1.0f, // 14
+            1.0f, -1.0f,  1.0f, // 15
+            -1.0f, -1.0f,  1.0f, // 16
+            -1.0f, -1.0f, -1.0f, // 17
+            -1.0f,  1.0f,  1.0f, // 18
+            -1.0f, -1.0f,  1.0f, // 19
+            1.0f, -1.0f,  1.0f, // 20
+            1.0f,  1.0f,  1.0f, // 21
+            1.0f, -1.0f, -1.0f, // 22
+            1.0f,  1.0f, -1.0f, // 23
+            1.0f, -1.0f, -1.0f, // 24
+            1.0f,  1.0f,  1.0f, // 25
+            1.0f, -1.0f,  1.0f, // 26
+            1.0f,  1.0f,  1.0f, // 27
+            1.0f,  1.0f, -1.0f, // 28
+            -1.0f,  1.0f, -1.0f, // 29
+            1.0f,  1.0f,  1.0f, // 30
+            -1.0f,  1.0f, -1.0f, // 31
+            -1.0f,  1.0f,  1.0f, // 32
+            1.0f,  1.0f,  1.0f, // 33
+            -1.0f,  1.0f,  1.0f, // 34
+            1.0f, -1.0f,  1.0f  // 35
         )
 
-        val rectIndices = intArrayOf(
-            0, 1, 2, 2, 3, 0
+        val indices = intArrayOf(
+            0, 1, 2, // Triangle 1
+            3, 4, 5, // Triangle 2
+            6, 7, 8, // Triangle 3
+            9, 10, 11, // Triangle 4
+            12, 13, 14, // Triangle 5
+            15, 16, 17, // Triangle 6
+            18, 19, 20, // Triangle 7
+            21, 22, 23, // Triangle 8
+            24, 25, 26, // Triangle 9
+            27, 28, 29, // Triangle 10
+            30, 31, 32, // Triangle 11
+            33, 34, 35  // Triangle 12
         )
 
         val rectLayout = renderer.choose<IBufferLayout>().create(
@@ -64,9 +107,9 @@ class Sandbox : Application(
             )
         )
 
-        rectVertexBuffer = renderer.choose<IVertexBuffer>().create(rectVertices, rectLayout)
+        rectVertexBuffer = renderer.choose<IVertexBuffer>().create(vertices, rectLayout)
 
-        rectIndexBuffer = renderer.choose<IIndexBuffer>().create(rectIndices)
+        rectIndexBuffer = renderer.choose<IIndexBuffer>().create(indices)
 
         rectVertexArray.addVertexBuffer(rectVertexBuffer)
         rectVertexArray.defineIndexBuffer(rectIndexBuffer)
