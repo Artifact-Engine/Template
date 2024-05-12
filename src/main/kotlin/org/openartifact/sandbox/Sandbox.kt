cc0f951a0ea928575a05dea9dbc1cfd3b1d1d3f8
meo209 <redslyice@outlook.de>
2024-05-12 22:14:12 +0200

    feat: A very simple 3D Projection!

--- src/main/kotlin/org/openartifact/sandbox/Sandbox.kt
+++ src/main/kotlin/org/openartifact/sandbox/Sandbox.kt
@@ -1,73 +1,42 @@
 package org.openartifact.sandbox
 
-import DataType
+import glm_.glm
+import glm_.mat4x4.Mat4
+import glm_.vec3.Vec3
 import glm_.vec4.Vec4
 import org.lwjgl.glfw.GLFW
-import org.lwjgl.opengl.GL46.GL_FRAGMENT_SHADER
-import org.lwjgl.opengl.GL46.GL_VERTEX_SHADER
-import org.openartifact.artifact.core.ApplicationEntry
-import org.openartifact.artifact.core.Application
-import org.openartifact.artifact.core.Artifact
+import org.openartifact.artifact.core.*
+import org.openartifact.artifact.graphics.DataType
 import org.openartifact.artifact.graphics.RenderAPI
 import org.openartifact.artifact.graphics.choose
 import org.openartifact.artifact.graphics.flow.renderFlow
 import org.openartifact.artifact.graphics.interfaces.*
 import org.openartifact.artifact.graphics.platform.opengl.OpenGLRenderer
-import org.openartifact.artifact.graphics.platform.opengl.OpenGLShader
 import org.openartifact.artifact.input.KeyConstants.KEY_LEFT_CONTROL
 import org.openartifact.artifact.input.KeyConstants.KEY_Q
 import org.openartifact.artifact.input.createKeyInputMap
 import org.openartifact.artifact.input.with
-import javax.xml.crypto.Data
 
 @ApplicationEntry
 @Suppress("unused")
 class Sandbox : Application(RenderAPI.OpenGL) {
 
     private val keyInputMap = createKeyInputMap {
-        KEY_LEFT_CONTROL with KEY_Q to { GLFW.glfwSetWindowShouldClose(Artifact.instance.window.handle, true) }
+        KEY_LEFT_CONTROL with  KEY_Q to { GLFW.glfwSetWindowShouldClose(Artifact.instance.window.handle, true) }
     }
 
-    private lateinit var triangleShader : IShader
     private lateinit var rectShader : IShader
-
-    private lateinit var triangleVertexArray : IVertexArray
-    private lateinit var triangleVertexBuffer : IVertexBuffer
-    private lateinit var triangleIndexBuffer : IIndexBuffer
 
     private lateinit var rectVertexArray : IVertexArray
     private lateinit var rectVertexBuffer : IVertexBuffer
     private lateinit var rectIndexBuffer : IIndexBuffer
 
+    private lateinit var projectionMatrix : Mat4
+
     override fun init() {
-        logger.info("org.openartifact.sandbox.Sandbox init")
+        logger.info("Sandbox init")
 
         renderer = createRenderer()
-
-        // Triangle
-        triangleVertexArray = renderer.choose<IVertexArray>().create()
-
-        val triangleVertices = floatArrayOf(
-            -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
-            0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-            0.0f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f
-        )
-
-        val triangleIndices = intArrayOf(0, 1, 2)
-
-        val triangleLayout = renderer.choose<IBufferLayout>().create(
-            mapOf(
-                DataType.Vec3 to "a_Position",
-                DataType.Vec4 to "a_Color"
-            )
-        )
-
-        triangleVertexBuffer = renderer.choose<IVertexBuffer>().create(triangleVertices, triangleLayout)
-
-        triangleIndexBuffer = renderer.choose<IIndexBuffer>().create(triangleIndices)
-
-        triangleVertexArray.addVertexBuffer(triangleVertexBuffer)
-        triangleVertexArray.defineIndexBuffer(triangleIndexBuffer)
 
         // Rectangle
         rectVertexArray = renderer.choose<IVertexArray>().create()
@@ -94,56 +63,18 @@ class Sandbox : Application(RenderAPI.OpenGL) {
         rectVertexArray.addVertexBuffer(rectVertexBuffer)
         rectVertexArray.defineIndexBuffer(rectIndexBuffer)
 
-        // Shader
-        val triangleVertexSource = """
-            #version 330 core
-            
-            layout(location = 0) in vec3 a_Position;
-            layout(location = 1) in vec4 a_Color;
-            
-            out vec3 v_Position;
-            out vec4 v_Color;
-            
-            void main() {
-                v_Position = a_Position;
-                gl_Position = vec4(a_Position, 1.0);
-                v_Color = a_Color;
-            }
-            
-        """.trimIndent()
-
-        val triangleFragmentSource = """
-            #version 330 core
-            
-            layout(location = 0) out vec4 color;
-            
-            in vec3 v_Position;
-            in vec4 v_Color;
-            
-            void main() {
-                color = v_Color;
-            }
-        """.trimIndent()
-
-        triangleShader = renderer.choose<IShader>(
-            triangleVertexSource,
-            triangleFragmentSource,
-        ).create()
-
-        // Shader2
         val rectangleVertexSource = """
             #version 330 core
             
             layout(location = 0) in vec3 a_Position;
 
             uniform vec4 u_Color;
+            uniform mat4 u_MVP;
             
-            out vec3 v_Position;
             out vec4 v_Color;
             
             void main() {
-                v_Position = a_Position;
-                gl_Position = vec4(a_Position, 1.0);
+                gl_Position = u_MVP * vec4(a_Position, 1.0);
                 v_Color = u_Color;
             }
             
@@ -154,7 +85,6 @@ class Sandbox : Application(RenderAPI.OpenGL) {
             
             layout(location = 0) out vec4 color;
             
-            in vec3 v_Position;
             in vec4 v_Color;
             
             void main() {
@@ -166,6 +96,9 @@ class Sandbox : Application(RenderAPI.OpenGL) {
             rectangleVertexSource,
             rectangleFragmentSource
         ).create()
+
+
+        projectionMatrix = glm.perspective(glm.radians(45f), 300f / 300f, 0.1f, 100f)
     }
 
     override fun update() {
@@ -174,20 +107,30 @@ class Sandbox : Application(RenderAPI.OpenGL) {
         (renderer as OpenGLRenderer).clearScreenBuffers()
 
         renderFlow {
+
+            val view = glm.lookAt(
+                Vec3(4, 3, 3),
+                Vec3(0, 0, 0),
+                Vec3(0, 1, 0)
+            )
+
+            val model = Mat4(1f)
+
+            val mvp = projectionMatrix * view * model
+
             directCommit(rectShader) {
+                parameterMat4("u_MVP", mvp)
                 parameterVec4("u_Color", Vec4(1f, 1f, 1f, 1f))
             }
 
             commit(rectVertexArray)
-
-            commit(triangleShader, triangleVertexArray)
 
             push()
         }
     }
 
     override fun shutdown() {
-        logger.info("org.openartifact.sandbox.Sandbox shutdown")
+        logger.info("Sandbox shutdown")
     }
 
 }
