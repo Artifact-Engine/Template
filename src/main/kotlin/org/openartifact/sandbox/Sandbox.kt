6a0c76e6703506337c160841b238e2a908ac812c
meo209 <redslyice@outlook.de>
2024-06-15 13:01:40 +0200

    feat: Basic model-loading

--- src/main/kotlin/org/openartifact/sandbox/Sandbox.kt
+++ src/main/kotlin/org/openartifact/sandbox/Sandbox.kt
@@ -12,18 +12,14 @@ package org.openartifact.sandbox
 
 import glm_.vec2.Vec2
 import glm_.vec3.Vec3
-import org.openartifact.artifact.utils.calculateModelMatrix
 import org.openartifact.artifact.core.application.Application
 import org.openartifact.artifact.core.event.events.FPSUpdateEvent
 import org.openartifact.artifact.core.event.events.ResizeEvent
 import org.openartifact.artifact.core.event.subscribe
-import org.openartifact.artifact.extensions.multiValuedMapOf
 import org.openartifact.artifact.extensions.reset
 import org.openartifact.artifact.graphics.*
 import org.openartifact.artifact.graphics.cameras.PerspectiveCamera
-import org.openartifact.artifact.graphics.interfaces.*
-import org.openartifact.artifact.graphics.platform.opengl.OpenGLShader
-import org.openartifact.artifact.graphics.platform.opengl.ShaderType
+import org.openartifact.artifact.graphics.mesh.Mesh
 import org.openartifact.artifact.graphics.window.WindowConfig
 import org.openartifact.artifact.input.*
 import org.openartifact.artifact.input.MouseInput.hold
@@ -38,117 +34,28 @@ class Sandbox : Application(
     )
 ) {
 
-    private lateinit var camera : PerspectiveCamera
     private val movement = Vec3()
 
     private val cameraInputMap = createKeyInputMap {
-        KEY_W to { movement.z += -1f }
-        KEY_A to { movement.x += -1f }
+        KEY_W to { movement.z += - 1f }
+        KEY_A to { movement.x += - 1f }
         KEY_S to { movement.z += 1f }
         KEY_D to { movement.x += 1f }
 
         KEY_E to { movement.y += 1f }
-        KEY_Q to { movement.y += -1f }
+        KEY_Q to { movement.y += - 1f }
     }
 
-    inner class Cube(val pos : Vec3) {
-        var shader : IShader
-
-        var vertexArray : IVertexArray
-        var vertexBuffer : IVertexBuffer
-        var indexBuffer : IIndexBuffer
-
-
-        val vertices = floatArrayOf(
-            -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
-            0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
-            0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
-            0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
-            -0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
-            -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
-
-            -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
-            0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
-            0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
-            0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
-            -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
-            -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
-
-            -0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
-            -0.5f, 0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
-            -0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
-            -0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
-            -0.5f, -0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
-            -0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
-
-            0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
-            0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-            0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-            0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-            0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
-            0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
-
-            -0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
-            0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
-            0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
-            0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
-            -0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
-            -0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
-
-            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-            0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-            0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
-            0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
-            -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
-            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f
-        )
-
-        val indices = intArrayOf()
-
-        init {
-            val bufferLayout = renderer.choose<IBufferLayout>().create(
-                multiValuedMapOf(
-                    DataType.Vec3 to "a_Position",
-                    DataType.Vec3 to "a_Normal"
-                )
-            )
-
-            vertexBuffer = renderer.choose<IVertexBuffer>().create(vertices, bufferLayout)
-
-            indexBuffer = renderer.choose<IIndexBuffer>().create(indices)
-
-            vertexArray = renderer.choose<IVertexArray>().create(
-                vertexBuffer,
-                indexBuffer
-            )
-
-            val vertex = resource("shaders/vertex.glsl")
-            val fragment = resource("shaders/fragment.glsl")
-
-            shader = renderer.choose<IShader>()
-                .create(
-                    listOf(
-                        OpenGLShader.ShaderModule(vertex.asText(), ShaderType.VERTEX),
-                        OpenGLShader.ShaderModule(fragment.asText(), ShaderType.FRAGMENT),
-                    )
-                )
-        }
-    }
-
-    private lateinit var cube1 : Cube
+    private lateinit var mesh : Mesh
 
     override fun init() {
         logger.info("Sandbox init")
 
         renderer = createRenderer()
 
-        cube1 = Cube(Vec3(1, 0, 0))
+        mesh = Mesh(resource("meshes/airboat.obj"))
 
-        camera = PerspectiveCamera(90f, Vec3(4, 4, 4), Vec3(22.5f, -45, 0))
-
-        subscribe(FPSUpdateEvent::class) { event ->
-            logger.info("FPS: ${event.fps}")
-        }
+        camera = PerspectiveCamera(90f, Vec3(4, 4, 4), Vec3(22.5f, - 45, 0))
 
         subscribe(ResizeEvent::class) { event ->
             logger.info("Resized to ${event.x} ${event.y}")
@@ -178,27 +85,12 @@ class Sandbox : Application(
         movement.reset()
 
         if (lightPosY > 5.0f || lightPosY < 0f) {
-            lightPosDirection = -lightPosDirection
+            lightPosDirection = - lightPosDirection
         }
         lightPosY += lightPosDirection
 
         renderer.frame {
-            renderFlow {
-                cube1.apply {
-                    commit(shader) {
-                        parameterMat4("u_Projection", camera.calculateProjectionMatrix())
-                        parameterMat4("u_View", camera.calculateViewMatrix())
-                        parameterMat4("u_Model", calculateModelMatrix(pos, Vec3(0, 0, 0)))
-                        parameterVec3("u_Color", Vec3(.6, .6, .6))
-                        parameterVec3("u_Light_Pos", Vec3(2, lightPosY, 0))
-                        parameterVec3("u_Light_Color", Vec3(.8, .8, .8))
-                    }
-
-                    commit(vertexArray)
-
-                    push()
-                }
-            }
+            commit(mesh)
         }
     }
 
